a304bdfe9650c356e93c8c4fb403289e243a5466
Greg Doud <gdoud@gmail.com>
2022-11-14 14:15:15 -0500

    Committed by GitUtil

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -14,7 +14,22 @@ export class EmployeeService {
     private http: HttpClient
   ) { }
 
+  login(email: string, password: string): Observable<Employee> {
+    return this.http.get(`${this.baseurl}/${email}/${password}`) as Observable<Employee>;
+  }
   list(): Observable<Employee[]> {
     return this.http.get(`${this.baseurl}`) as Observable<Employee[]>;
+  }
+  get(id: number): Observable<Employee> {
+    return this.http.get(`${this.baseurl}/${id}`) as Observable<Employee>;
+  }
+  create(empl: Employee): Observable<Employee> {
+    return this.http.post(`${this.baseurl}`, empl) as Observable<Employee>;
+  }
+  change(empl: Employee): Observable<any> {
+    return this.http.put(`${this.baseurl}/${empl.id}`, empl) as Observable<any>;
+  }
+  remove(id: number): Observable<any> {
+    return this.http.delete(`${this.baseurl}/${id}`) as Observable<any>;
   }
 }
